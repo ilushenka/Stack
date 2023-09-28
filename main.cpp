36c1377ccc43180133587b2488012f6dc331f7a7
ILUHAAAA <pramskii.is@phystech.edu>
2023-09-28 22:42:05 +0300

    added hash protection

--- main.cpp
+++ main.cpp
@@ -4,6 +4,13 @@ typedef unsigned long long canary_t;
 
 #include "stack.h"
 
+#ifdef DEBUG
+static void new_hash(Stack *stk);
+static int hash_calc(const void *stk, int stk_size);
+static bool check_struct_hash_equality(Stack *stk);
+static bool check_array_hash_equality(Stack *stk);
+#endif
+
 int main() {
     elem_t ret_value = 0;
     Stack stk = {};
@@ -11,83 +18,9 @@ int main() {
     for(int i = 0; i < 50; i++)
         stack_push(&stk, i);
     stack_pop(&stk, &ret_value);
-    STACK_DUMP(&stk, 0);
+    STACK_DUMP(&stk, 1023);
 
     stack_destroy(&stk);
-}
-
-#ifdef DEBUG
-int stack_init(Stack *stk, size_t capacity, const char *file_name,
-                 const char *func_name, int line_num) {
-    stk->data = (elem_t *)calloc(capacity * sizeof(elem_t) +
-                 2 * sizeof(canary_t), sizeof(char));
-    stk->data = (elem_t *)((canary_t *)stk->data + 1);
-    stk->size = 0;
-    stk->capacity = capacity;
-    stk->stk_inf.file_name = file_name;
-    stk->stk_inf.func_name = func_name;
-    stk->stk_inf.line_num = line_num;
-    stk->left_canary = canary_val;
-    stk->right_canary = canary_val;
-    *((canary_t *)stk->data - 1) = canary_val;
-    *((canary_t *)(stk->data + capacity)) = canary_val;
-
-    int errors = stack_ok(stk);
-
-    if(errors) 
-        STACK_DUMP(stk, errors);
-
-        return errors;
-}
-
-static int stack_realloc(Stack *stk) {
-    int errors = stack_ok(stk);
-    if(errors) {
-        STACK_DUMP(stk, errors);
-        return errors;
-    }
-    stk->data = (elem_t *)((char *)stk->data - sizeof(canary_t));
-
-    if(stk->size == stk->capacity) {
-        stk->capacity *= capacity_multiplier;
-        stk->data = (elem_t *)realloc(stk->data, 
-                    (stk->capacity * sizeof(elem_t) + 2 * sizeof(canary_t)));
-        *((canary_t *)((char *)stk->data +
-        stk->capacity * sizeof(elem_t) + sizeof(canary_t))) = 
-        *((canary_t *)((char *)stk->data +
-        stk->capacity/capacity_multiplier * sizeof(elem_t) + sizeof(canary_t)));
-
-    }
-    else if(stk->capacity > capacity_multiplier * 2 * stk->size &&
-             stk->capacity > capacity) {
-        stk->capacity /= capacity_multiplier;
-        
-        *((canary_t *)((char *)stk->data +
-        stk->capacity * sizeof(elem_t) + sizeof(canary_t))) =   
-        *((canary_t *)((char *)stk->data +
-        stk->capacity * capacity_multiplier * sizeof(elem_t) + sizeof(canary_t)));
-        
-        stk->data = (elem_t *)realloc(stk->data,
-                     (stk->capacity * sizeof(elem_t) + 2 * sizeof(canary_t)));
-    }
-    
-    stk->data = (elem_t *)((char *)stk->data + sizeof(canary_t));
-
-   errors = stack_ok(stk);
-    if(errors)
-        STACK_DUMP(stk, errors);
-    return errors;
-}
-
-#endif
-
-int stack_destroy(Stack *stk) {
-    free((canary_t *)stk->data - 1);
-    stk->size = 0;
-    stk->capacity = 0;
-    stk = NULL;
-    
-    return 0;
 }
 
 int stack_push(Stack *stk, elem_t new_elem) {
@@ -101,6 +34,9 @@ int stack_push(Stack *stk, elem_t new_elem) {
     }
     stk->data[stk->size] = new_elem;
     ++stk->size;
+#ifdef DEBUG
+    new_hash(stk);
+#endif    
     errors = stack_ok(stk);
 #ifdef DEBUG
     if(errors)
@@ -126,14 +62,25 @@ int stack_pop(Stack *stk, elem_t *ret_value) {
     *ret_value = stk->data[stk->size];
     stk->data[stk->size] = POISON;
 
+#ifdef DEBUG
+    new_hash(stk);
+#endif
     stack_realloc(stk);
-    
    errors = stack_ok(stk);
 #ifdef DEBUG
     if(errors)
         STACK_DUMP(stk, errors);
 #endif
     return errors;
+}
+
+int stack_destroy(Stack *stk) {
+    free((canary_t *)stk->data - 1);
+    stk->size = 0;
+    stk->capacity = 0;
+    stk = NULL;
+    
+    return 0;
 }
 
 #ifndef DEBUG
@@ -166,10 +113,8 @@ static int stack_realloc(Stack *stk) {
     return stack_ok(stk);
 }
 #endif
-void set_poison(Stack *stk) {
 
-}
-int stack_ok(const Stack *stk) {
+int stack_ok(Stack *stk) {
     int errors = 0;
     if(!stk)
         errors |= empty_struct;
@@ -188,38 +133,155 @@ int stack_ok(const Stack *stk) {
         errors |= invalid_left_arr_canary;
     if(*((canary_t *)(stk->data + stk->capacity)) != canary_val)
         errors |= invalid_right_arr_canary;
+    if(!(check_struct_hash_equality(stk)))
+        errors |= invalid_struct_hash;
+    if(!(check_array_hash_equality(stk)))
+        errors |= invalid_array_hash;
+
 #endif
     
     return errors;
 }
 
 #ifdef DEBUG
-int stack_dump(const Stack *stk, const char *stk_name, const char *file_name,
+
+Error_info err_inf[] = {
+{empty_struct,                  "The structure has pointer on null.\n"},
+{empty_stack,                   "Stack has pointer on null "
+                                "(empty and doesn't have free memory).\n"},
+{invalid_size_or_capacity,      "Stack capacity lesser than stack size.\n"},
+{invalid_capacity,              "Stack capacity has invalid number.\n"},
+{invalid_left_struct_canary,    "Left canary in struct has invalid value\n"},
+{invalid_right_struct_canary,   "Right canary in struct has invalid value\n"},
+{invalid_left_arr_canary,       "Left canary in array has invalid value\n"},
+{invalid_right_arr_canary,      "Right canary in array has invalid value\n"},
+{invalid_struct_hash,           "Hash in structure has invalid value\n"},
+{invalid_array_hash,            "Hash in array has invalid value\n"},
+
+};
+
+static void new_hash(Stack *stk) {
+    stk->hash_array = 0;
+    stk->hash_struct = 0;
+    stk->hash_struct = hash_calc(stk, sizeof(*stk));
+    stk->hash_array = hash_calc((canary_t *)stk->data - 1, 
+                        sizeof(canary_t) * 2 + sizeof(elem_t) * stk->capacity);
+}
+
+static bool check_struct_hash_equality(Stack *stk) {
+    int old_struct_hash = stk->hash_struct;
+    int old_array_hash = stk->hash_array;
+
+    stk->hash_array = 0;
+    stk->hash_struct = 0;
+    bool is_equal = (old_struct_hash == hash_calc(stk, sizeof(*stk)));
+    stk->hash_array = old_array_hash;
+    stk->hash_struct = old_struct_hash;
+    return is_equal;
+}
+
+static bool check_array_hash_equality(Stack *stk) {
+    return stk->hash_array == hash_calc((canary_t *)stk->data - 1,
+                        sizeof(canary_t) * 2 + sizeof(elem_t) * stk->capacity);
+}
+
+static int hash_calc(const void *stk, int stk_size) {
+    const int multiplier = 69;
+    const char *str = (const char *)stk;
+    int hash = 0;
+    for(int i = 0; i < stk_size; ++i) {
+        hash += *(str + i);
+        hash *= multiplier;
+    }
+    return hash;
+}
+
+int stack_init(Stack *stk, size_t capacity, const char *file_name,
+                 const char *func_name, int line_num) {
+    stk->data = (elem_t *)calloc(capacity * sizeof(elem_t) +
+                 2 * sizeof(canary_t), sizeof(char));
+    stk->data = (elem_t *)((canary_t *)stk->data + 1);
+    stk->size = 0;
+    stk->capacity = capacity;
+    stk->stk_inf.file_name = file_name;
+    stk->stk_inf.func_name = func_name;
+    stk->stk_inf.line_num = line_num;
+    stk->left_canary = canary_val;
+    stk->right_canary = canary_val;
+    *((canary_t *)stk->data - 1) = canary_val;
+    *((canary_t *)(stk->data + capacity)) = canary_val;
+    
+    new_hash(stk);
+    
+    int errors = stack_ok(stk);
+
+    if(errors) 
+        STACK_DUMP(stk, errors);
+
+    return errors;
+}
+
+static int stack_realloc(Stack *stk) {
+    int errors = stack_ok(stk);
+    if(errors) {
+        STACK_DUMP(stk, errors);
+        return errors;
+    }
+    stk->data = (elem_t *)((char *)stk->data - sizeof(canary_t));
+
+    if(stk->size == stk->capacity) {
+        stk->capacity *= capacity_multiplier;
+        stk->data = (elem_t *)realloc(stk->data, 
+                    (stk->capacity * sizeof(elem_t) + 2 * sizeof(canary_t)));
+        *((canary_t *)((char *)stk->data +
+        stk->capacity * sizeof(elem_t) + sizeof(canary_t))) = 
+        *((canary_t *)((char *)stk->data +
+        stk->size * sizeof(elem_t) + sizeof(canary_t)));
+        memset(((char *)stk->data + stk->size * sizeof(elem_t) +
+                sizeof(canary_t)), 0, (stk->capacity - stk->size) *
+                sizeof(elem_t));
+    }
+    else if(stk->capacity > capacity_multiplier * 2 * stk->size &&
+             stk->capacity > capacity) {
+        stk->capacity /= capacity_multiplier;
+        
+        *((canary_t *)((char *)stk->data +
+        stk->capacity * sizeof(elem_t) + sizeof(canary_t))) =   
+        *((canary_t *)((char *)stk->data +
+        stk->capacity * capacity_multiplier * sizeof(elem_t) +
+                                            sizeof(canary_t)));
+        
+        stk->data = (elem_t *)realloc(stk->data,
+                    (stk->capacity * sizeof(elem_t) +
+                     2 * sizeof(canary_t)));
+    }
+    
+    stk->data = (elem_t *)((char *)stk->data + sizeof(canary_t));
+
+    new_hash(stk);
+    errors = stack_ok(stk);
+    if(errors)
+        STACK_DUMP(stk, errors);
+    return errors;
+}
+
+int stack_dump(Stack *stk, const char *stk_name, const char *file_name,
                 int line_num, const char *func_name, int errors) {
-    if(errors & empty_struct)
-        printf("ERROR: the structure has pointer on null.\n");
-    if(errors & empty_stack)
-        printf("ERROR: stack has pointer on null(empty and doesn't have free memory).\n");
-    if(errors & invalid_size_or_capacity)
-        printf("ERROR: stack capacity lesser than stack size.\n");
-    if(errors & invalid_capacity)
-        printf("ERROR: stack capacity has invalid number.\n");
-    if(errors & invalid_left_struct_canary)
-        printf("ERROR: left canary in struct has invalid value\n");
-    if(errors & invalid_right_struct_canary)
-        printf("ERROR: right canary in struct has invalid value\n");
-    if(errors & invalid_left_arr_canary)
-        printf("ERROR: left canary in array has invalid value\n");
-    if(errors & invalid_right_arr_canary)
-        printf("ERROR: right canary in array has invalid value\n");
+    for(int i = 0; i < sizeof(err_inf) / sizeof(err_inf[0]); i++) {
+        if(errors & err_inf[i].err_num)
+            printf("%s", err_inf[i].err_descr);
+    }
     printf("Stack[%p] \"%s\" from %s(%d) %s.\n", stk, stk_name,
-             stk->stk_inf.func_name, stk->stk_inf.line_num, stk->stk_inf.file_name);
+    stk->stk_inf.func_name, stk->stk_inf.line_num, stk->stk_inf.file_name);
     printf("Called from %s(%d) %s\n", func_name, line_num, file_name);
-    printf("{\n");
     printf("left canary in struct %0llX\n", stk->left_canary);
     printf("right canary in struct %0llX\n", stk->right_canary);
     printf("left canary in array %0llX\n", *((canary_t *)stk->data - 1));
-    printf("right canary in array %0llX\n",*((canary_t *)(stk->data + stk->capacity)));
+    printf("right canary in array %0llX\n",
+        *((canary_t *)(stk->data + stk->capacity)));
+    printf("hash in structure %d\n", stk->hash_struct);
+    printf("hash in array %d\n", stk->hash_array);
+    printf("{\n");
     printf("size = %ld\n", stk->size);
     printf("capacity = %ld\n", stk->capacity);
     printf("data[%p]\n", stk->data);
--- stack.h
+++ stack.h
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <malloc.h>
 #include <limits.h>
+#include <cstring>
 
 #define DEBUG
 
@@ -20,6 +21,8 @@ typedef struct {
 typedef struct {
 #ifdef DEBUG
     canary_t left_canary;
+    int hash_struct;
+    int hash_array;
 #endif
     elem_t *data;
     size_t size;
@@ -30,21 +33,41 @@ typedef struct {
 #endif
 }Stack;
 
+enum errors {
+    empty_struct = 1,
+    empty_stack = 2,
+    invalid_size_or_capacity = 4,
+    invalid_capacity = 8,
+#ifdef DEBUG
+    invalid_left_struct_canary = 16,
+    invalid_right_struct_canary = 32,
+    invalid_left_arr_canary = 64,
+    invalid_right_arr_canary = 128,
+    invalid_struct_hash = 256,
+    invalid_array_hash = 512
+#endif
+};
+
 #ifdef DEBUG
 
 int stack_init(Stack *stk, size_t capacity, const char *file_name,
                  const char *func_name, int line_num);
 
 #define STACK_DUMP(stk, errors) stack_dump(stk, #stk, __FILE__, \
-                                            __LINE__, __func__, errors)
+                                            __LINE__, __PRETTY_FUNCTION__, errors)
 
 #define STACK_INIT(stk, capacity) stack_init(stk, capacity, __FILE__, \
                                          __PRETTY_FUNCTION__, __LINE__ - 1)
 
 const unsigned long long canary_val = 0xD15C0C001;
 
-int stack_dump(const Stack *stk, const char *stk_name, const char *file_name, 
+int stack_dump(Stack *stk, const char *stk_name, const char *file_name, 
                 int line_num, const char *func_name, int errors);
+
+typedef struct {
+    errors err_num;
+    const char *err_descr;
+}Error_info;
 
 #endif
 
@@ -56,19 +79,6 @@ int stack_init(Stack *stk, size_t capacity);
 
 #endif
 
-enum errors {
-    empty_struct = 1,
-    empty_stack = 2,
-    invalid_size_or_capacity = 4,
-    invalid_capacity = 8,
-#ifdef DEBUG
-    invalid_left_struct_canary = 16,
-    invalid_right_struct_canary = 32,
-    invalid_left_arr_canary = 64,
-    invalid_right_arr_canary = 128
-#endif
-};
-
 static int stack_realloc(Stack *stk);
 
 const size_t capacity = 25;
@@ -79,4 +89,4 @@ int stack_destroy(Stack *stk);
 int stack_push(Stack *stk, elem_t new_elem);
 int stack_pop(Stack *stk, elem_t *ret_value);
 
-int stack_ok(const Stack *stk);
+int stack_ok(Stack *stk);
